0085cea81f5575709e8502fa910e206732506753
Josleke <josleke@gmail.com>
2026-03-27 12:57:49 +0100

    Added common platform

---
+++ srta-common-platform/src/main/java/org/meldtech/platform/srta/common/SrtaCommonPlatformApplication.java
@@ -0,0 +1,13 @@
+package org.meldtech.platform.srta.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrtaCommonPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrtaCommonPlatformApplication.class, args);
+    }
+
+}
---
+++ srta-common-platform/src/test/java/org/meldtech/platform/srta/common/SrtaCommonPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meldtech.platform.srta.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrtaCommonPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
